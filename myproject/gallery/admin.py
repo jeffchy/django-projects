74490b164e5e3acf8773af9cabfff4ce23186523
jeff jiang <jeff jiang>
2017-02-05 19:55:29 +0800

    rm

--- myproject/gallery/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Imginfo
-
-
-admin.site.register(Imginfo)
--- myproject/gallery/static/js/addurl.js
+++
@@ -1,6 +0,0 @@
-$("#id_qiniu_url").focus(function(){
-  console.log('hahahahahahaha');
-  var get_url = $(".info").find("a").attr("href");
-  console.log(get_url);
-  $(this).attr('value',get_url);
-});
